7daf94b6b4076855a0dcd1b708a22ba4200b918d
Samuel Barabas <samuel@owee.de>
2026-07-13 23:54:27 +0200

    somad v0.14.0

--- Formula/somad.rb
+++ Formula/somad.rb
@@ -1,22 +1,23 @@
 class Somad < Formula
   desc "Client for streaming SomaFM radio channels"
   homepage "https://github.com/samuelb/somad"
-  version "0.13.0"
+  version "0.14.0"
   license "MIT"
 
   on_macos do
-    url "https://github.com/samuelb/somad/releases/download/v0.13.0/soma_darwin_universal"
-    sha256 "b17657e01dda2af15fea917d2ad63d47c05a739df10c56ff5953731a7356d7c5"
+    url "https://github.com/samuelb/somad/releases/download/v#{version}/soma_darwin_universal"
+    # Release automation replaces this placeholder with the published checksum.
+    sha256 "f8c3177dc80192717452def0d0644974a63186616db168e3b19085e17b356f86"
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/samuelb/somad/releases/download/v0.13.0/soma_linux_arm64"
-      sha256 "3333a6422d1b16ea8f2de128326f4e7299e1d100f57c471722725665b053b310"
+      url "https://github.com/samuelb/somad/releases/download/v#{version}/soma_linux_arm64"
+      sha256 "d7cbd4ebc37f96728692b4c9799690bdad7c5a900dcf80e949656ca3d656c06e"
     end
     on_intel do
-      url "https://github.com/samuelb/somad/releases/download/v0.13.0/soma_linux_amd64"
-      sha256 "d29cffc41f28f80de5d456f08ab41f179710d82743348ba62b51f7107c3f950a"
+      url "https://github.com/samuelb/somad/releases/download/v#{version}/soma_linux_amd64"
+      sha256 "b158be2feae5b5e2237b3fa046eb2a8cc99903f7a9bc39d72b4dcfa1a9b7a77f"
     end
   end
 
